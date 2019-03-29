b94fee62fbdbd5815af5cf52305b5000b5ce62a2
alvaro <asanchezsanchez@webimpacto.es>
2019-03-29 08:44:16 +0100

    WEB-1669 cambios estructurales

--- wim_objectlogguer.php
+++ wim_objectlogguer.php
@@ -23,11 +23,10 @@
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
-require_once 'classes/ObjectLogguer.php';
-
 if (!defined('_PS_VERSION_'))
     exit;
+
+require_once 'classes/ObjectLogguer.php';
 
 class Wim_Objectlogguer extends Module
 {
